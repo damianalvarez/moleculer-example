2e2a9c175648c42896b7f64a627877aa7d457601
Damián Alvarez <dalvarez@humancaresystems.com>
2019-09-24 00:36:00 -0300

    Add sub action in math service

--- services/math.service.js
+++ services/math.service.js
@@ -6,9 +6,11 @@ module.exports = {
     dependencies: [],
     actions: {
         add(ctx) {
-            console.log('params', ctx.params);
             return Number(ctx.params.a) + Number(ctx.params.b);
-        }
+        },
+        sub(ctx) {
+            return Number(ctx.params.a) - Number(ctx.params.b);
+        },
     },
     events: {},
     methods: {},
